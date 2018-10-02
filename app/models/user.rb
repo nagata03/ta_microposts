33d51e9947194e0d91bafe987dcb0bfdbdc7cbb3
s.nagata <nagata_sho@kakaku.com>
2018-10-02 03:22:50 +0000

    Favoriteモデル作成、Userモデルにメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
   has_secure_password # パスワード付きのモデルの場合に利用するとよい
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   
   def follow(other_user)  # 他のユーザをフォローする
     unless self == other_user
@@ -27,6 +31,21 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
   
+  def like(micropost)
+    unless self == micropost.user
+      self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+  end
+  
+  def unlike(micropost)
+    favorites = self.favorites.find_by(micropost_id: micropost.id)
+    favorites.destroy if favorites
+  end
+
+  def like?(micropost)
+    self.likes.include?(micropost)
+  end
+
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
